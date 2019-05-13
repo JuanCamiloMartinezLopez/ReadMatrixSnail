d66ca03404531a7fd83a8bf18dcbcd9af5fc52af
CAMILO <juacmartinezl@correo.udistrital.edu.co>
2019-05-12 22:32:01 -0500

    se termina

--- matriz.py
+++ matriz.py
@@ -1,2 +1,16 @@
+import numpy as np
+
+def printFirstNumber(matrix):
+    print matrix[0]
+
+def printNumber(matrix):
+    printFirstNumber(matrix)
+
+    if len(matrix[1:,:]) is not 0:
+        printNumber(np.rot90(matrix[1:, :]))
+
 if __name__=="__main__":
-    print ([x.split() for x in open("fichero.txt").readlines()])
+    printNumber(np.array([x.split() for x in open("fichero.txt").readlines()]))
+    
+
+
